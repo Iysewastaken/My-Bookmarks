fb4dad00f681dd2127820ed203057d2825455c13
Iyse <92163853+Iysewastaken@users.noreply.github.com>
2021-10-13 17:39:29 +0400

    Added links but its still doesnt work
    I'm Slowly Dying

---
+++ glance.js
@@ -0,0 +1,147 @@
+var https = "https://";
+var firstlinkvar;
+var secondlinkvar;
+var thirdlinkvar;
+var fourthlinkvar;
+var fifthlinkvar;
+var sixthlinkvar;
+var seventhlinkvar;
+var eighthlinkvar;
+var ninethlinkvar;
+var tenthlinkvar;
+
+// ^^ variables ^^
+
+function Zoomlinks() {
+    document.getElementById("1").innerHTML = "Homeroom - Ms April";
+    var firstlinkvar = "us04web.zoom.us/j/2434972513?pwd=jq0cQdedLMKpLt4jJoibH0NmvP9w3A&fbclid=IwAR32n8e65yXWSF9ETodbLUaU_6k3D_TQpct4o9DdJDSq3iGFicf8V8jIy78";
+
+    document.getElementById("2").innerHTML = "Social/Moral - Ms Cathy";
+    var secondlinkvar = "https://us04web.zoom.us/j/6040988054?pwd=dHRaOHhkZnNrVTBFTytWdHVIUytNdz09";
+
+    document.getElementById("3").innerHTML = "English - Sir Jeff";
+    var thirdlinkvar = "https://us04web.zoom.us/j/9318061146?pwd=czFYbkxQdXRINGhMLzN0QXA3RG5GUT09&fbclid=IwAR3GQCqqnG6di_0hNectF1bVZKNHIcN0AxQVKEmcGTqc_LujzvHhf-yEJ2c";
+    
+    document.getElementById("4").innerHTML = "Filipino - Ms Yvette";
+    var fourthlinkvar = "https://us04web.zoom.us/j/2434972513?pwd=jq0cQdedLMKpLt4jJoibH0NmvP9w3A&fbclid=IwAR32n8e65yXWSF9ETodbLUaU_6k3D_TQpct4o9DdJDSq3iGFicf8V8jIy78";
+    
+    document.getElementById("5").innerHTML = "Math - Ms April";
+    var fifthlinkvar = "https://us04web.zoom.us/j/378064746?pwd=OE9Ca3RwM21lMGt0SFpHT004cGdOQT09&fbclid=IwAR1oaX290OZ3IEvFSY9YCOhXOujOSdVHgm_cnFzZEfw8LErxG8RFgizLSKk";
+    
+    document.getElementById("6").innerHTML = "Science/ESP - Sir Ronald";
+    var sixthlinkvar = "https://us05web.zoom.us/j/6444413366?pwd=K1dLbzBqU0VmbDR4M3M0b1JJa3B6QT09&fbclid=IwAR0jruzbJFUH1ywICTguHZaA-bYIfRBiMqCIs6c6oaN41OMAtOgDQqTIdzs";
+    
+    document.getElementById("7").innerHTML = "Mapeh/TLE - Ms Virgie";
+    var seventhlinkvar = "https://us04web.zoom.us/j/2819115648?pwd=SUtycDd4M3FvbmZiSkJVUzVxOXpBUT09&fbclid=IwAR0k9UCNQ6pPOA4bNH-gBNu8TUkC02QTAKIO_pSTQaGCXeN4Yt0nu28irH4";
+    
+    document.getElementById("8").innerHTML = "Computer - Sir Abdul";
+    var eighthlinkvar = "https://us04web.zoom.us/j/5111896804?pwd=KzBvdXI2cDZvYlF3MzlGQitsbk5QZz09&fbclid=IwAR2oAz7C4hzEpVmIlW-OLDeOE5-7rAntF0Gq0gWOYmYAHLyrym3m1NAxGgI";
+    
+    document.getElementById("9").innerHTML = "Arabic - Ms Zeinab";
+    var ninethlinkvar = "https://us04web.zoom.us/j/9815605919?pwd=d1BHLy9vY3B2TmNsMVZIbE5DUzBDQT09https://www.google.com/";
+    
+    document.getElementById("10").innerHTML = "Current Morning Exercise";
+    var tenthlinkvar = "https://www.youtube.com/watch?v=MS6Au02VPFI";
+    
+    console.log("zoomlinks has been deployed");
+  }
+
+// ^^ Zoom Links ^^ <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+function Bookmarks() {
+    document.getElementById("1").innerHTML = "Home Link 1";
+    var firstlinkvar = "https://www.youtube.com/";
+
+    document.getElementById("2").innerHTML = "Zoom Link 2";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("3").innerHTML = "Zoom Link 3";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("4").innerHTML = "Zoom Link 4";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("5").innerHTML = "Zoom Link 5";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("6").innerHTML = "Zoom Link 6";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("7").innerHTML = "Zoom Link 7";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("8").innerHTML = "Zoom Link 8";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("9").innerHTML = "Zoom Link 9";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("10").innerHTML = "Zoom Link 10";
+    var firstlinkvar = "https://www.google.com/";
+    
+    console.log("homelinks has been deployed");
+  }
+
+// ^^ Zoom Links ^^ <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+function Procrastinate() {
+    document.getElementById("1").innerHTML = "Procrastination Link 1";
+    var firstlinkvar = "https://www.github.com/";
+
+    document.getElementById("2").innerHTML = "Zoom Link 2";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("3").innerHTML = "Zoom Link 3";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("4").innerHTML = "Zoom Link 4";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("5").innerHTML = "Zoom Link 5";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("6").innerHTML = "Zoom Link 6";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("7").innerHTML = "Zoom Link 7";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("8").innerHTML = "Zoom Link 8";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("9").innerHTML = "Zoom Link 9";
+    var firstlinkvar = "https://www.google.com/";
+    
+    document.getElementById("10").innerHTML = "Zoom Link 10";
+    var firstlinkvar = "https://www.google.com/";
+    
+    console.log("proctrastination links has been deployed");
+  }
+
+// ^^ Zoom Links ^^ <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// vv side buttons vv
+function link1() {location.href = https + firstlinkvar}
+
+function link2() {location.href = secondlinkvar}
+
+function link3() {location.href = thirdlinkvar}
+
+function link4() {location.href = fourthlinkvar}
+
+function link5() {location.href = fifthlinkvar}
+
+function link6() {location.href = sixthlinkvar}
+
+function link7() {location.href = seventhlinkvar}
+
+function link8() {location.href = eighthlinkvar}
+
+function link9() {location.href = ninethlinkvar}
+
+function link10() {location.href = tenthlinkvar}
+
+
+// ^^ side buttons ^^
+
+
